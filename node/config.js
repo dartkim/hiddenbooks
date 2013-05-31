0daf34e824ad7c57c8e33cf1c2e5bff6054490c2
carpenta <viruscih@gmail.com>
2013-05-31 23:17:22 +0900

    evernote client nodejs 용 코드 추가

---
+++ node/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+		
+	 evernoteConsumerKey		: 'carpenta'
+	,evernoteConsumerSecret	: '06109e9ed60b52de'
+	,evernoteUsedSandbox		: true
+	
+	,serverUrl:'http://localhost:8081'
+	,serverPort:8081,
+}
